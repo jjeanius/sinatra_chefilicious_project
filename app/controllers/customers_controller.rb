167b9e1a968e58c9d40746d3ff40211aa5461e15
Judy McGoldrick <jjeanius@verizon.net>
2018-09-29 16:56:02 +0000

    correct customer.name to name in signup

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -10,7 +10,13 @@ class CustomersController < ApplicationController
   end
 
   post '/signup' do     #    signup/get request create post
-    @customer = Customer.new(params)
+    @customer = Customer.new(:name =>params[:name], :username =>params[:usrname], :email =>params[:email], :password =>params[:password])
+    if @customer save
+      @customer.id = session[:customer_id]
+        redirect "/show"
+    else
+        redirect "/signup"
+    end
   end
 
 
