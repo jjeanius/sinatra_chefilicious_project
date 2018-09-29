02e6a045affbb42a57bac0c90c5871edec973f73
Judy McGoldrick <jjeanius@verizon.net>
2018-09-29 06:22:17 +0000

    fixing of in model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ActiveRecord::Base
   has_many :meal_kits
   has_secure_password
-  validates_presence of :username, :email, :customer
+  validates_presence_of :username, :email
 
 
 
