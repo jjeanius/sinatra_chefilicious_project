289311431b5dd354167e674329a0a79f9f15b684
Judy McGoldrick <jjeanius@verizon.net>
2018-10-28 04:27:52 +0000

    adding update view

--- app/controllers/authenticate_controller.rb
+++ app/controllers/authenticate_controller.rb
@@ -31,9 +31,9 @@ class AuthenticateController < ApplicationController
   end
 
    post '/login' do     #    post request - '/login' route
-     @customer = Customer.find_by(:username =>params[:username])
-       if @customer && @customer.authenticate(params[:password])
-         session[:customer_id] = @customer.id  # if customer authenticate / we set the customer id
+     customer = Customer.find_by(:username =>params[:username])
+       if customer && customer.authenticate(params[:password])
+         session[:customer_id] = customer.id  # if customer authenticate / we set the customer id
 
          flash[:message] = " Successfully Login!"
          redirect "/main_menu"   # and redirect to main_menu
