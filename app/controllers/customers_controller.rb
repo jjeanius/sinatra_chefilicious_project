7be19156dd6073d6ad2e40afa7b2ffa7fb9eb214
Judy McGoldrick <jjeanius@verizon.net>
2018-10-29 02:24:27 +0000

    fixing bug in customer account

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,6 +4,11 @@ class CustomersController < ApplicationController
 
   use Rack::Flash
 
+
+
+
+
+
   get '/main_menu' do      # Get request  - show action
     erb :'/customers/main_menu'    # rendering " main_menu"
   end
@@ -18,15 +23,10 @@ class CustomersController < ApplicationController
   end
 
   get '/customers/:id' do
-    if logged_in?
-      @customer = Customer.find_by(params[:customer_id])
-      session[:customer_id] = @customer.id
-      mealkits = Mealkit.all
-      @customer.mealkits.each do|mealkit|
-        mealkit
-      end
-      erb :'/customers/show'
-    end
+    @customer = Customer.find(session[:customer_id])
+    mealkits = Mealkit.all
+    @customer.mealkits
+    erb :'/customers/show'
   end
 
 end
