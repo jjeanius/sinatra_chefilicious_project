735a5135ba34219d6c243c0bc00dc2c851773ca2
Judy McGoldrick <jjeanius@verizon.net>
2018-10-30 05:02:02 +0000

    working on customer update & delete

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -9,37 +9,38 @@ class CustomersController < ApplicationController
   end
 
   get '/customers' do  # get request, "customer route", show all action
-    if logged_in?
       @customer = Customer.find_by(params[:customer_id])
-      session[:customer_id] = @customer.id
+    #  session[:customer_id] = @customer.id
       @customers = Customer.all
       erb :'/customers/index'
     end
-  end
-
   get '/customers/:id/edit' do   # get request, "/customers/:id/edit" route, edit action
     @customer = Customer.find(session[:customer_id])
       @customers = Customer.all
     erb :'/customers/edit'
   end
 
-  patch '/customers/' do
-    binding.pry
-    @customer = Customer.find(params[:customer_id])
+  patch '/customers/:id' do
+    @customer = Customer.find_by(params[:id])
     @customer.update(name: params[:name], email: params[:email])
     @customer.save
     flash[:message] = "Successfully updated!"
     redirect to "/customers/#{@customer_id}"
   end
 
-#  get '/customers/:id' do     #get request, "customer dynamic route" show 1 action
-#    @customer = Customer.find(session[:customer_id])
-#    mealkits = Mealkit.all
-#    @customer.mealkits
-#    erb :'/customers/show'
-#  end
 
+  get '/customers/:id' do     #get request, "customer dynamic route" show 1 action
+    @customer = Customer.find(session[:customer_id])
+    mealkits = Mealkit.all
+    @customer.mealkits
+    erb :'/customers/show'
+  end
 
+  delete '/customers/:id/delete' do
+    @customer = Customer.find_by_id(params[:id])
+      @customer.delete
+    redirect to '/customers'
+  end
 
 
 end
