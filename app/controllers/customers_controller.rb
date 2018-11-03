73c46313624373cdfe7800300e42f1782965961e
Judy McGoldrick <jjeanius@verizon.net>
2018-11-03 17:51:10 +0000

    adding delete button in customer index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,40 +8,9 @@ class CustomersController < ApplicationController
     erb :'/customers/main_menu'    # rendering " main_menu"
   end
 
-  get '/customers' do  # get request, "customer route", show all action
-      @customer = Customer.find_by(params[:customer_id])
-    #  session[:customer_id] = @customer.id
-      @customers = Customer.all
-      erb :'/customers/index'
-    end
-  get '/customers/:id/edit' do   # get request, "/customers/:id/edit" route, edit action
-    @customer = Customer.find_by(params[:id])
-      @customers = Customer.all
-    erb :'/customers/edit'
-  end
-
-  patch '/customers/' do
-    binding.pry
-    if logged_in? && current_customer
-      @customer = Customer.find_by(params[:id])
-      @customer.update(name: params[:name], email: params[:email])
-      @customer.save
-      flash[:message] = "Successfully updated!"
-      redirect to "/customers/#{@customer_id}"
-    end
-  end
-
-#  get '/customers/:id' do     #get request, "customer dynamic route" show 1 action
-#    @customer = Customer.find_by(params[:id])
-#    mealkits = Mealkit.all
-#    @customer.mealkits
-#    erb :'/customers/show'
-#  end
-
-  delete '/customers/:id/delete' do
-    @customer = Customer.find_by_id(params[:id])
-      @customer.delete
-    redirect to '/customers'
+  get '/customers' do   #  get request "/customers" route - showing all customers action
+    @customers = Customer.all  # allow the view to access all the customer names in the db through the instance variable @customers
+    erb :'/customers/index'
   end
 
 
--- app/views/mealkits/customer.rb
+++ app/views/mealkits/customer.rb
@@ -2,5 +2,5 @@ class Customer < ActiveRecord::Base  # holds logic of the app/ AR provides objec
   has_many :mealkits
   has_secure_password   # authenticate user / ac macros give access to few method- secure passwords
   validates_presence_of :username, :email, :password
-
+  attr_accessor :mealkit, :mealkits
 end
