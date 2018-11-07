2823f3eef92dace83909a0ce4175e583be3530d8
Judy McGoldrick <jjeanius@verizon.net>
2018-11-07 20:08:30 +0000

    changes in params

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -12,30 +12,44 @@ class CustomersController < ApplicationController
     end
   end
 
+  get '/customers/:id' do     #get request, "customer dynamic route" show 1 action
+    if logged_in?
+      @customer = current_customer
+      params[:id] = session[:customer_id]
+      @customer = Customer.find_by(id: params[:id])
+          mealkits = Mealkit.all
+        @customer.mealkits
+      erb :'/customers/show'
+    end
+  end
+
   get '/customers' do   #  get request "/customers" route - showing all customers action
     if logged_in?
-      @customer = Customer.find_by(id: params[:id])
+      @customer = current_customer
         @customers = Customer.all  # allow the view to access all the customer names in the db through the instance variable @customers
       erb :'/customers/index'
     end
   end
 
+
 get '/customers/:id/edit' do   # get request, "/customers/:id/edit" route, load the edit form
   if logged_in?
-  #  @customer = Customer.find_by(id: session[:customer_id])
-  @customer = Customer.find_by(id: params[:id])
-    erb :'/customers/edit'
+    @customer = current_customer
+      params[:id] = session[:customer_id]
+        @customer = Customer.find_by(id: params[:id])
+      erb :'/customers/edit'
   end
-
 end
 
  patch '/customers/:id' do    # patch request / edit action /
+   #binding.pry
     @customer = Customer.find_by(id: params[:id])
-    binding.pry
-    @customer.update(name: params["customer"]["name"],email: params["customer"]["email"])
+    @customer.name = params["customer"]["name"]
+    @customer.email = params["customer"]["email"]
     @customer.save
     redirect to "/customers/#{@customer.id}"
   end
+
 
   delete '/customers/:id/delete' do
   #  binding.pry
@@ -43,16 +57,5 @@ end
       @customer.delete
     redirect to '/customers'
   end
-
-  get '/customers/:id' do     #get request, "customer dynamic route" show 1 action
-    if logged_in?
-        @customer = Customer.find(session[:customer_id])
-          mealkits = Mealkit.all
-        @customer.mealkits
-      erb :'/customers/show'
-    end
-  end
-
-
 
 end
