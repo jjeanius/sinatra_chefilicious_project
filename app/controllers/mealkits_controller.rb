10dd378df4d6048a932b1fe28bacfcd39ee9c0e1
Judy McGoldrick <jjeanius@verizon.net>
2018-11-15 22:42:18 +0000

    adding index page in mealkits

--- app/controllers/mealkits_controller.rb
+++ app/controllers/mealkits_controller.rb
@@ -4,6 +4,20 @@ class MealkitsController < ApplicationController
 
   use Rack::Flash
 
+  get '/mealkits' do   #  get request / show all mealkit action
+    if logged_in? && current_customer
+        @customer = Customer.find(session[:customer_id])
+        @mealkits = Mealkit.all   # collect all mealkits
+      erb :'/mealkits/index'
+    else
+      redirect "/login"
+    end
+  end
+
+
+
+
+
   get '/mealkits/new' do     # get requst - '/request to load the form for new mealkit
     if logged_in?
       current_customer
@@ -13,14 +27,13 @@ class MealkitsController < ApplicationController
     end
   end
 
-  post '/mealkits' do    # post request / it responds to the post request
-    if logged_in?
-      current_customer
+  post '/mealkits' do    # post request to create new mealkit
+    if logged_in? && current_customer
+      @mealkit = Mealkit.find_by(id: params[:id])
       @mealkits = Mealkit.all
-    #  @mealkits.save
 
       flash[:message] = "Successfully created a Meal Kit!"
-      redirect to ("/mealkits/create")
+      redirect to ("/mealkits/show")
     else
       flash[:message] = "Please login!"
       redirect to "authenticate/login"
@@ -38,8 +51,7 @@ class MealkitsController < ApplicationController
   end
 
   get '/mealkits/:id/edit' do    # get request/ load edit action   1/2
-    if logged_in?
-      @customer = current_customer
+    if logged_in? && current_customer
         mealkits = Mealkit.all
           @mealkit = Mealkit.find_by(params[:id])
         erb :'/mealkits/edit'
@@ -51,28 +63,19 @@ class MealkitsController < ApplicationController
   patch '/mealkits/:id' do   #  patch request / edit action 2/2 it needs to match action of form submitted for get mealkit/edit
     if logged_in?
       current_customer
+
         @mealkit = Mealkit.find_by(id: params[:id])   #now id is no longer nil because it is part of line 59
         @mealkit.update(name: params[:mealkit][:name], ingredients: params[:mealkit][:ingredients], time: params[:mealkit][:time], serving_size: params[:mealkit][:serving_size])
         @mealkit.save
 
         flash[:message] = "Successfully updated!"
-        redirect to ("/mealkits/by_customer")
+        redirect to ("customer/#{@customer.id}")
     else
         flash[:message] = "Please login!"
         redirect to "authenticate/login"
     end
   end
 
-    get '/mealkits' do   #  get request / show all mealkit action
-      if logged_in?
-        @customer = current_customer
-          @customer = Customer.find(session[:customer_id])
-          @mealkits = Mealkit.all   # collect all mealkits
-        erb :'/mealkits/index'
-      else
-        redirect "//login"
-      end
-    end
 
     get '/mealkits/:id' do   # get request / show/ dynamic routing - accessing view through the params hash
       if logged_in?
